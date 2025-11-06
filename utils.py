1d3a1d999e9644e532fbb6d49cbc97e37db303ad
Bachtiar Mardiansyah <134371373+BachtiarM12@users.noreply.github.com>
2025-11-06 09:44:28 +0700

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,18 @@
+import pickle
+from sklearn.compose import ColumnTransformer
+
+
+def get_model():
+    with open("lgbm_model.pkl", "rb") as file:
+        loaded_model = pickle.load(file)
+    return loaded_model
+
+def get_prediction(data):
+    model = get_model()
+    pred_class = list(model.predict(data))[0]
+    pred_prob = list(model.predict_proba[:, 1])[0]
+    return {
+        "class": pred_class,
+        "probability": pred_prob
+    }
+
